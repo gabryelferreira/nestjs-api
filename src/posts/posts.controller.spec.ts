d0555121e3caa5f176555c8ccc97ebffce6a299f
Gabryel <gabryel.ferreira@iteris.com.br>
2020-05-09 18:28:41 -0300

    Added unit test to posts controller

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -2,8 +2,11 @@ import { PostsController } from './posts.controller';
 import { Connection } from 'typeorm';
 import { PostsService } from './posts.service';
 import { createMemDB } from '../utils/testing-helpers/createMemDB';
-import { createPostsService, createUsersService } from '../utils/testing-helpers/commonServices';
-import { UsersService } from 'src/users/users.service';
+import { createPostsService, createUsersService, createCommonRequest } from '../utils/testing-helpers/commonServices';
+import { UsersService } from '../users/users.service';
+import { createUser } from '../utils/testing-helpers/createUser';
+import * as faker from 'faker';
+import { CreatePostDTO } from '../models/dto/post.dto';
 
 describe('Posts Controller', () => {
   let db: Connection;
@@ -23,4 +26,33 @@ describe('Posts Controller', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create post', async () => {
+    const user = await usersService.create(createUser());
+
+    const post: CreatePostDTO = {
+      text: faker.lorem.sentences()
+    };
+
+    const request = createCommonRequest(user);
+
+    const result = await controller.create(request, post);
+    expect(result.text).toBe(post.text);
+  });
+
+  it('should fail creating post without user', async () => {
+    const post: CreatePostDTO = {
+      text: faker.lorem.sentences()
+    };
+    expect.assertions(1);
+
+    const request = createCommonRequest();
+
+    try {
+      await controller.create(request, post);
+    } catch (e) {
+      expect(e).not.toBeNull();
+    }
+  });
+
 });
