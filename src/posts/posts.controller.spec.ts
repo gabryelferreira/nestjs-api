c7310b20d53f18fefb81d9ed7b6943158de1b7d7
Gabryel <gabryel.ferreira@iteris.com.br>
2020-05-09 15:03:22 -0300

    Testing controller

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,16 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
+import { Connection } from 'typeorm';
+import { PostsService } from './posts.service';
+import { createMemDB } from '../utils/testing-helpers/createMemDB';
+import { createPostsService, createUsersService } from '../utils/testing-helpers/commonServices';
+import { UsersService } from 'src/users/users.service';
 
 describe('Posts Controller', () => {
+  let db: Connection;
+  let postsService: PostsService;
+  let usersService: UsersService;
   let controller: PostsController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [PostsController],
-    }).compile();
-
-    controller = module.get<PostsController>(PostsController);
+  beforeAll(async () => {
+    db = await createMemDB();
+    postsService = await createPostsService(db);
+    usersService = await createUsersService(db);
+    controller = new PostsController(postsService, usersService);
   });
+
+  afterAll(() => db.close());
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,49 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { Connection } from 'typeorm';
+import { UsersService } from './users.service';
+import { createMemDB } from '../utils/testing-helpers/createMemDB';
+import { AuthService } from '../auth/auth.service';
+import { commonAuthService, createUsersService, createCommonRequest } from '../utils/testing-helpers/commonServices';
+import { createUser } from '../utils/testing-helpers/createUser';
 
 describe('Users Controller', () => {
-  let controller: UsersController;
+  let db: Connection;
+  let usersController: UsersController;
+  let usersService: UsersService;
+  let authService: AuthService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
-
-    controller = module.get<UsersController>(UsersController);
+    db = await createMemDB();
+    usersService = await createUsersService(db);
+    authService = commonAuthService;
+    usersController = new UsersController(usersService, authService);
   });
 
+  afterEach(() => db.close());
+
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
+  });
+
+  it('should create user', async () => {
+
+    const user = createUser();
+
+    const result = await usersController.create(user);
+    expect(result.user.id).toBe(1);
+    expect(result.user.name).toBe(user.name);
+    expect(result.user.username).toBe(user.username);
+    expect(result.user.password).toBeUndefined();
+  });
+
+  it("should retrieve users except me", async () => {
+    const myUser = await usersService.create(createUser());
+    const anotherUser = await usersService.create(createUser());
+
+    const request = createCommonRequest(myUser);
+
+    const result = await usersController.getUsers(request);
+    expect(!!result.find(user => user.name === myUser.name)).toBeFalsy();
+    expect(result.find(user => user.username === anotherUser.username)).toBeTruthy();
   });
 });
