0478400b10c4c6c8594a9b557b2ac92cf3e07de7
Gabryel <gabryel.ferreira@iteris.com.br>
2020-05-09 15:06:45 -0300

    Added @BeforeInsert method to hash the password

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,24 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
-import { IsNotEmpty } from 'class-validator';
-import { Post } from "src/posts/post.entity";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, BeforeInsert, BaseEntity } from "typeorm";
+import { Post } from "../posts/post.entity";
+import bcryptHelper from "../utils/bcrypt-helper";
 
 
 @Entity()
-export class User {
+export class User extends BaseEntity {
 
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column({ nullable: false })
-    @IsNotEmpty()
     name: string;
 
     @Column({ nullable: false, unique: true })
-    @IsNotEmpty()
     username: string;
 
     @Column({ nullable: false })
-    @IsNotEmpty()
     password: string;
 
     @OneToMany(() => Post, post => post.user)
@@ -32,5 +29,10 @@ export class User {
 
     @Column({ name: "updated_at", default: Date.now() })
     updatedAt: Date;
+
+    @BeforeInsert()
+    async updatePassword() {
+        this.password = await bcryptHelper.hash(this.password);
+    }
 
 }
