8e24bfb1a3bd1fd90fceff23930c418cb57d7ba7
Gabryel <gabryel.ferreira@iteris.com.br>
2020-05-09 15:01:10 -0300

    Changed bcrypt functionalities to src/utils/bcrypt-helper.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/users/user.entity';
 import { IPayload } from 'src/models/payload.model';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,19 +12,6 @@ export class AuthService {
   getAccessToken(user: User): string {
     const payload: IPayload = { username: user.username, id: user.id };
     return this.jwtService.sign(payload);
-  }
-
-  async hashPassword(password: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      bcrypt.hash(password, 10, (err: Error, hash: string) => {
-        if (err) reject(err);
-        else resolve(hash);
-      })
-    })
-  }
-
-  async comparePasswords(password: string, hash: string): Promise<boolean> {
-    return await bcrypt.compare(password, hash);
   }
 
 }
